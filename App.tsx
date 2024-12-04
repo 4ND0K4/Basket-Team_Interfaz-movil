99f4dafe01a00f2540dcd600727116bc57e7e66e
4ND0K4 <162052218+angdoca@users.noreply.github.com>
2024-12-04 02:57:11 +0100

    Primeros pasos con navigation
    - Instalaciones iniciales de react navigation con buen resultado

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native';
 
 import Navbar from './src/components/shared/Navbar';
@@ -7,11 +8,13 @@ import CreatePlayerScreen from './src/screens/CreatePlayerScreen';
 
 export const App = () => {
   return (
+    <NavigationContainer>
       <SafeAreaView style={{ flex: 1}}>
         <Navbar />
         {/*<PlayerListScreen /> */}
         <CreatePlayerScreen />
       </SafeAreaView>
+      </NavigationContainer>
   )
 }
 
